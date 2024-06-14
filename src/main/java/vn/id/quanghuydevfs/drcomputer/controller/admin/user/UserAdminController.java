0427de06197ac0e8472b8a97da5647ef7063e658
BuiQuangHuy <20130276@st.hcmuaf.edu.vn>
2024-06-14 12:24:12 +0700

    refactor CrossOrrigin

--- src/main/java/vn/id/quanghuydevfs/drcomputer/controller/admin/user/UserAdminController.java
+++ src/main/java/vn/id/quanghuydevfs/drcomputer/controller/admin/user/UserAdminController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/management/users")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin("*")
 //@PreAuthorize("hasRole('ADMIN')")
 public class UserAdminController {
     private final UserService userService;
